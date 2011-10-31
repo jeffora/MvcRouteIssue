0a2d17f044d5e8a8044a8fcbda6269586e318771
Jeff Burn <jeff@jeffburn.com>
2011-10-31 23:08:08 +1000

    initial commit demonstrating issue

---
+++ Controllers/AvailabilityServiceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRouteIssue.Controllers
+{
+    public class AvailabilityServiceController : Controller
+    {
+        //
+        // GET: /AvailabilityService/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
